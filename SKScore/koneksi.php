3751bee211e3f473aa57de910cf1e555a50e146e
Feliks Lourensius <31313098+FeliksLo09@users.noreply.github.com>
2019-12-04 23:28:43 +0700

    Update koneksi.php
    New Format

--- SKScore/koneksi.php
+++ SKScore/koneksi.php
@@ -1,32 +1,10 @@
 <?php
 	$host="localhost";
-	$user="u3332521_makjang";
-	$pw="feliksl123";
-	$db="u3332521_sksman";
-	//$host="localhost";
-	//$user="root";
-	//$pw="";
-	//$db="dbkardikax";
+	$user=""; //Username
+	$pw=""; //Password
+	$db=""; //Database name
 
-	$koneksi=mysql_connect($host,$user,$pw) or die ("Koneksi gagal");
-	mysql_select_db($db, $koneksi) or die ("Gagal memilih database");
+	$koneksi=mysql_connect($host,$user,$pw) or die ("Failed Connection");
+	mysql_select_db($db, $koneksi) or die ("Failed to choose database");
 	
-	
-	//-------setup no-------------------------//
-    function transno($nomer){
-    	if(strlen($nomer)==1){
-    			$nomer = "0000".$nomer;
-    		}
-    	elseif(strlen($nomer)==2){
-    			$nomer = "000".$nomer;
-    		}
-    	elseif(strlen($nomer)==3){
-    			$nomer = "00".$nomer;
-    		}
-    	elseif(strlen($nomer)==4){
-    			$nomer = "0".$nomer;
-    		}
-    	return $nomer;
-    }
-//--------------------------------------//
 ?>
